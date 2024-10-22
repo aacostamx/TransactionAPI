ea666203ef3b03faaf715af7ebe062190565bdc3
Antonio Acosta <eltony162@gmail.com>
2024-10-22 13:03:52 -0700

    Add modifications

--- Program.cs
+++ Program.cs
@@ -3,8 +3,16 @@ using TransactionAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 
+// Set the HTTPS Redirection Options (set the HTTPS port)
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 7039; // Explicitly set the HTTPS port
+});
+
+// Configure the DbContext with Npgsql and migrations history table
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -12,7 +20,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     )
 );
 
-
+// Enable CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -21,21 +29,26 @@ builder.Services.AddCors(options =>
                           .AllowAnyMethod());
 });
 
+// Add Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
+// Enable Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Enforce HTTPS Redirection
 app.UseHttpsRedirection();
 
+// Enable CORS
 app.UseCors("AllowAll");
 
+// Enable Authorization
 app.UseAuthorization();
 
+// Map Controllers
 app.MapControllers();
 
 app.Run();
